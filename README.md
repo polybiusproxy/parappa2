5fefc0b798657ab0ca23c898222a474aab841d66
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2023-11-14 19:36:30 +0100

    everything

---
+++ README.md
@@ -0,0 +1,8 @@
+# PaRappa the Rapper 2 Decompilation
+
+### Setup
+
+1. Create a folder named `iso` in the root of the repo.
+2. Extract `SCPS_150.17` from an ISO of the July 12th prototype and place it in said `iso` folder.
+3. `./configure.py`
+4. `ninja`
---
+++ include/common.h
@@ -0,0 +1,31 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "include_asm.h"
+
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef long s64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+typedef float f32;
+
+#define UNK_TYPE s32
+#define UNK_PTR void*
+#define UNK_RET void
+#define UNK_FUN_ARG void(*)(void)
+#define UNK_FUN_PTR(name) void(*name)(void)
+#define UNK_ARGS
+
+#define NULL 0
+#define TRUE 1
+#define FALSE 0
+
+#define ARRAY_COUNT(arr) (s32)(sizeof(arr) / sizeof(arr[0]))
+
+#endif /* COMMON_H */
