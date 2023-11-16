c2879965a2ef1ec4918d0a07f51f649979ccd8a8
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2023-11-16 18:57:59 +0100

    try to split C code

--- include/include_asm.h
+++ include/include_asm.h
@@ -2,6 +2,7 @@
 #define INCLUDE_ASM_H
 
 #if !defined(SPLAT) && !defined(__CTX__) && !defined(PERMUTER)
+
 #ifndef INCLUDE_ASM
 #define INCLUDE_ASM_INTERNAL(TYPE, BASE_FOLDER, FOLDER, NAME, ARGS...) \
     __asm__(                                                           \
@@ -15,9 +16,27 @@
         "\t.end\t" #NAME);
 #define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...) INCLUDE_ASM_INTERNAL(TYPE, "nonmatchings", FOLDER, NAME, ARGS)
 #endif
+
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA_INTERNAL(TYPE, BASE_FOLDER, FOLDER, NAME, ARGS...) \
+    __asm__(                                                              \
+	    ".section .rodata\n"                                              \
+		"\t.include \"asm/" BASE_FOLDER "/" FOLDER "/" #NAME ".s\"\n"     \
+		".section .text");
+#define INCLUDE_RODATA(TYPE, FOLDER, NAME, ARGS...) INCLUDE_RODATA_INTERNAL(TYPE, "nonmatchings", FOLDER, NAME, ARGS)
+#endif
+
 __asm__(".include \"include/macro.inc\"\n");
+
 #else
+
+#ifndef INCLUDE_ASM
 #define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...)
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(TYPE, FOLDER, NAME, ARGS...)
+#endif
+
 #endif
 
 #endif /* INCLUDE_ASM_H */
---
+++ src/dbug/dbgmsg.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100130);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_001001C8);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100338);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_001003D0);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100420);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100438);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100490);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_001005B8);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_001006D0);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_001007F0);
+
+INCLUDE_ASM(const s32, "dbug/dbgmsg", func_00100830);
+
+INCLUDE_RODATA(const s32, "dbug/dbgmsg", D_00390F00);
+
+INCLUDE_RODATA(const s32, "dbug/dbgmsg", D_00390F10);
