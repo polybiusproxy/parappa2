a0b699b475d7e771fb2d0ab59d1cfe9005d6188b
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2023-11-20 20:36:01 +0100

    dbug: split `vramsave.c`

---
+++ src/dbug/vramsave.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "dbug/vramsave", BekiDat);
+
+INCLUDE_ASM(const s32, "dbug/vramsave", VramSave);
+
+INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMP);
+
+INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMPDouble);
+
+INCLUDE_ASM(const s32, "dbug/vramsave", VramTmpSave);
+
+INCLUDE_ASM(const s32, "dbug/vramsave", VramTmpSaveOutBMP);
