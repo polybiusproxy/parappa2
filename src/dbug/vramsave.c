83eb22cc7addf6d65ce394e02148afa4600b15fd
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2023-11-20 20:45:02 +0100

    dbug/vramsave: split `.data` and `.rodata`

--- src/dbug/vramsave.c
+++ src/dbug/vramsave.c
@@ -4,6 +4,10 @@ INCLUDE_ASM(const s32, "dbug/vramsave", BekiDat);
 
 INCLUDE_ASM(const s32, "dbug/vramsave", VramSave);
 
+INCLUDE_RODATA(const s32, "dbug/vramsave", D_00390F30);
+
+INCLUDE_RODATA(const s32, "dbug/vramsave", D_00390F48);
+
 INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMP);
 
 INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMPDouble);
@@ -11,3 +15,5 @@ INCLUDE_ASM(const s32, "dbug/vramsave", VramSaveBMPDouble);
 INCLUDE_ASM(const s32, "dbug/vramsave", VramTmpSave);
 
 INCLUDE_ASM(const s32, "dbug/vramsave", VramTmpSaveOutBMP);
+
+INCLUDE_RODATA(const s32, "dbug/vramsave", D_00390F80);
