c791e09ba8b891607f08922624c607a96deac66e
Daniel R <47796739+polybiusproxy@users.noreply.github.com>
2024-02-21 18:59:14 +0100

    os: match usrmem.c

---
+++ include/os/usrmem.h
@@ -0,0 +1,25 @@
+#ifndef USRMEM_H
+#define USRMEM_H
+
+#include "common.h"
+#include <eetypes.h>
+
+void UsrMemClear(void);
+void UsrMemClearTop(void);
+void UsrMemClearEnd(void);
+
+u_int UsrMemGetAdr(int id);
+u_int UsrMemGetSize(int id);
+u_int UsrMemGetEndAdr(int id);
+u_int UsrMemGetEndSize(int id);
+
+u_int UsrMemAllocNext(void);
+u_int UsrMemAllocEndNext(void);
+
+u_int UsrMemAlloc(int size);
+u_int UsrMemEndAlloc(int size);
+
+void UsrMemFree(void);
+void UsrMemEndFree(void);
+
+#endif
