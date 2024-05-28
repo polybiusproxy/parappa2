4be1a47ee3a2969d8a4527de5c602dfc50bb62b0
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2024-05-28 20:09:38 +0200

    main/cmnfile: split

---
+++ src/main/cmnfile.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "main/cmnfile", cmn_file_str_search);
+
+INCLUDE_ASM(const s32, "main/cmnfile", cmnfTim2Trans);
+
+INCLUDE_ASM(const s32, "main/cmnfile", cmnfGetFileAdrs);
+
+INCLUDE_ASM(const s32, "main/cmnfile", cmnfGetFileSize);
+
+INCLUDE_RODATA(const s32, "main/cmnfile", D_00393B48);
