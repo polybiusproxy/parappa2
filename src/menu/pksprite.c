d488234dad139bfc8d6b29ea90a0398296a07c1e
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2024-06-28 17:02:58 +0200

    menu/pksprite: match functions - PkMesh_Create - PkMesh_Delete - PkMesh_SetXYWH - PkMesh_SetUVWH - PkCMesh_Add - PkFTMesh_Add

--- src/menu/pksprite.c
+++ src/menu/pksprite.c
@@ -1,6 +1,7 @@
 #include "menu/pksprite.h"
 
 #include <eekernel.h>
+
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
@@ -29,8 +30,9 @@ static void _tsWorkEnd(TS_WORKMEM *emem)
 
 #if 1
 INCLUDE_ASM(const s32, "menu/pksprite", _tsWorkInit);
-static u_int* _tsWorkInit(/* s0 16 */ TS_WORKMEM *emem, /* s2 18 */ u_int *buf, /* s1 17 */ u_int size);
+static u_int* _tsWorkInit(TS_WORKMEM *emem, u_int *buf, u_int size);
 #else
+static u_int* _tsWorkInit(/* s0 16 */ TS_WORKMEM *emem, /* s2 18 */ u_int *buf, /* s1 17 */ u_int size)
 {
     u_int *tmp;
 
@@ -39,11 +41,11 @@ static u_int* _tsWorkInit(/* s0 16 */ TS_WORKMEM *emem, /* s2 18 */ u_int *buf, 
     if (buf == NULL)
     {
         if (size != 0)
-            tmp = memalign(128, size);
+            tmp = memalign(128, size * sizeof(u_long128));
         else
-            tmp = NULL;        
+            tmp = NULL;
 
-        //emem->top = tmp;
+        emem->top = tmp;
         emem->size = size;
         emem->isAlloc = 1;
     }
@@ -376,7 +378,162 @@ void _pkVU0RotMatrixZ(float rz)
     ");
 }
 
+#if 1
 INCLUDE_ASM(const s32, "menu/pksprite", PkRSprite_Add);
+#else
+void PkRSprite_Add(/* s3 19 */ SPR_PKT pk, /* s0 16 */ SPR_PRM *ppspr, /* s2 18 */ int flg)
+{
+    /* s1 17 */ SprTagTFR *sp = (SprTagTFR*)*pk;
+
+    _pkVU0RotMatrixZ(ppspr->rot);
+
+    *(u_long*)&sp->GifCord[0] = 0xa400000000008001;
+    *(u_long*)&sp->GifCord[2] = 0x4343434310;
+
+    sp->prim = 0x154;
+    sp->rgba = ppspr->rgba0;
+
+    if (flg & 1)
+    {
+        asm __volatile__("lq $9, 0x20(%0)" :: "r"(ppspr)); // ppspr->ux => $t1
+    }
+    else
+    {
+        asm __volatile__("lq $9,  0x0(%0)" :: "r"(ppspr));
+    }
+
+    asm __volatile__
+    ("
+        # t0 -> $8
+        # t1 -> $9
+        # t2 -> $10
+
+        lq     $8, 0x20(%0)     # ppspr->ux => $t0 ($8)
+        li     $10, -8
+        daddu  $9,  $0,  $8
+        pcpyld $8,  $8,  $0
+        paddw  $9,  $8,  $9
+        psllw  $9,  $9,  0x4
+
+        li     $8,  0x8         # t0 = 8
+        pextlw $8,  $10, $8
+        pextlw $8,  $8,  $8
+        paddw  $8,  $9,  $8
+        ppach  $9,  $0,  $8
+        sd     $9,  0x20(%1)    # sp->uv0
+
+        pexew  $10, $8
+        ppach  $8,  $0, $10
+        sd     $8,  0x30(%1)    # sp->uv1
+
+        prot3w $10, $8
+        sd     $10, 0x40(%1)    # sp->uv2
+
+        prot3w $10, $9
+        sd     $10, 0x50(%1)    # sp->uv3
+    " :: "r"(ppspr), "r"(sp));
+
+    asm __volatile__
+    ("
+        lqc2        vf01, 0x0(%0)
+        lq          $8,   0x10(%0)
+
+        vitof0.xyzw vf01, vf01
+
+        qmtc2       $8, vf02
+        pcpyud      $8, $8, $8
+
+        qmtc2       $8, vf03
+        lq          $8, 0x30(%0)
+        pcpyld      $8, $8, $8
+        qmtc2       $8, vf04
+        vmul.xy     vf04, vf04, vf03
+    " :: "r"(ppspr));
+
+    if (flg & 2 && ppspr->zoom.isOn)
+    {
+        asm __volatile__
+        ("
+            lq       $8, 0x0(%0)
+
+            qmtc2    $8, vf05
+            pcpyud   $8, $8, $8
+
+            qmtc2    $8, vf08
+            vadda.xy ACC,  vf01, vf0
+            vmadd.xy vf01, vf04, vf08
+            vmul.zw  vf01, vf01, vf05
+            vmul.zw  vf04, vf04, vf08
+            vsub.xy  vf01, vf01, vf05
+            vadda.xy ACC,  vf05, vf00
+            vmadd.xy vf01, vf01, vf08
+        " :: "r"(&ppspr->zoom));
+    }
+
+    asm __volatile__
+    ("
+        vadd.xy      vf01, vf01, vf04
+        vadd.xy      vf08, vf01, vf02
+        vmr32.xyzw   vf02, vf02
+        vmr32.xyzw   vf04, vf04
+        vmr32.xyzw   vf05, vf01
+        vmr32.xyzw   vf02, vf02
+        vmr32.xyzw   vf04, vf04
+        vmr32.xyzw   vf05, vf05
+
+        # t4 -> $12
+        lw           $12,  0x0(%0)
+        vmul.xy      vf06, vf06, vf02
+        vmul.xy      vf07, vf07, vf02
+        vsub.xy      vf09, vf00, vf04
+        vsub.y       vf10, vf00, vf04
+        vsub.x       vf10, vf05, vf04
+        vsub.x       vf11, vf00, vf04
+        vsub.y       vf11, vf05, vf04
+        vsub.xy      vf12, vf05, vf04
+        vmulax.xyzw  ACC,  vf06, vf09
+        vmadday.xyzw ACC,  vf07, vf09
+        vmaddw.xyzw  vf09, vf08, vf00
+        vmulax.xyzw  ACC,  vf06, vf10
+        vmadday.xyzw ACC,  vf07, vf10
+        vmaddw.xyzw  vf10, vf08, vf00
+        vftoi4.xyzw  vf09, vf09
+        vmulax.xyzw  ACC,  vf06, vf11
+        vmadday.xyzw ACC,  vf07, vf11
+        vmaddw.xyzw  vf11, vf08, vf00
+        vftoi4.xyzw  vf10, vf10
+
+        qmtc2        $8,   vf09
+        vmulax.xyzw  ACC,  vf06, vf12
+        vmadday.xyzw ACC,  vf07, vf12
+        vmaddw.xyzw  vf12, vf08, vf00
+        vftoi4.xyzw  vf11, vf11
+
+        qmfc2        $9,   vf10
+        ppach        $8,  $0,  $8
+        pextlw       $8,  $12, $8
+        sd           $8,  0x28(%1)
+
+        vftoi4.xyzw  vf12, vf12
+
+        ppach        $8,  $0,  $9
+        pextlw       $8,  $12, $8
+        sd           $8,  0x38(%1)
+
+        qmfc2        $10, vf11
+        ppach        $8,  $0,  $10
+        pextlw       $8,  $12, $8
+        sd           $8,  0x48(%1)
+
+        qmfc2        $11, vf12
+        ppach        $8, $0,  $11
+        pextlw       $8, $12, $8
+        sd           $8, 0x58(%1)
+    " :: "r"(ppspr->zdepth), "r"(sp));
+
+    *pk = sp->GifCord;
+}
+#endif
 
 INCLUDE_ASM(const s32, "menu/pksprite", PkCLine2_Add);
 
@@ -387,22 +544,213 @@ INCLUDE_ASM(const s32, "menu/pksprite", PkCLineS_AddNext);
 INCLUDE_ASM(const s32, "menu/pksprite", PkPolyF3_Add);
 
 INCLUDE_ASM(const s32, "menu/pksprite", PkPolyF4_Add);
+void PkPolyF4_Add(/* t3 11 */ SPR_PKT pk, /* a1 5 */ SPR_PRM *ppspr, /* a2 6 */ int flg);
 
 INCLUDE_ASM(const s32, "menu/pksprite", PkPolyFT4_Add);
+void PkPolyFT4_Add(/* t3 11 */ SPR_PKT pk, /* a1 5 */ SPR_PRM *ppspr, /* a2 6 */ int flg);
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_Create);
+PKMESH* PkMesh_Create(int w, int h)
+{
+    PKMESH *pmesh = malloc(sizeof(PKMESH));
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_Delete);
+    if (pmesh == NULL)
+        return NULL;
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_SetXYWH);
+    memset(pmesh, 0, sizeof(PKMESH));
+    pmesh->pmspt = malloc(sizeof(PKMSPT) * (w + 1) * (h + 1));
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_SetUVWH);
+    if (pmesh->pmspt == NULL)
+    {
+        free(NULL);
+        return NULL;
+    }
+    else
+    {
+        pmesh->mw = w;
+        pmesh->mh = h;
+    }
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkCMesh_Add);
+    return pmesh;
+}
 
-INCLUDE_ASM(const s32, "menu/pksprite", PkFTMesh_Add);
+void PkMesh_Delete(PKMESH *mesh)
+{
+    if (mesh == NULL)
+        return;
+    
+    if (mesh->pmspt != NULL)
+        free(mesh->pmspt);
 
+    free(mesh);
+}
+
+void PkMesh_SetXYWH(PKMESH *mesh, float px0, float py0, float sw, float sh)
+{
+    PKMSPT *pt;
+    int     x, y;
+    float   fmw, fmh;
+    float   py;
+
+    pt = mesh->pmspt;
+
+    mesh->px = px0;
+    mesh->py = py0;
+    
+    mesh->sw = sw;
+    mesh->sh = sh;
+
+    fmw = 1.0f / mesh->mw;
+    fmh = 1.0f / mesh->mh;
+
+    for (y = 0; y < (mesh->mh + 1); y++)
+    {
+        py = py0 + ((y * sh) * fmh);
+
+        for (x = 0; x < (mesh->mw + 1); x++, pt++)
+        {
+            pt->x = px0 + ((x * sw) * fmw);
+            pt->y = py;
+
+            pt->ofsx = pt->ofsy = 0.0f;
+        }
+    }
+}
+
+void PkMesh_SetUVWH(PKMESH *mesh, float ux0, float uy0, float uw, float uh)
+{
+    PKMSPT *pt;
+    int     x, y;
+    float   fmw, fmh;
+    int     uy;
+
+    pt = mesh->pmspt;
+
+    fmw = 1.0f / mesh->mw;
+    fmh = 1.0f / mesh->mh;
+
+    for (y = 0; y < (mesh->mh + 1); y++)
+    {
+        uy = uy0 + ((y * uh) * fmh);
+
+        for (x = 0; x < (mesh->mw + 1); x++, pt++)
+        {
+            pt->u = ux0 + ((x * uw) * fmw);
+            pt->v = uy;         
+        }
+    }
+}
+
+void PkCMesh_Add(SPR_PKT pk, SPR_PRM *spr, PKMESH *mesh)
+{
+    int     cidx, lidx;
+    int     x, y;
+    PKMSPT *pt;
+
+    cidx = 0;
+    lidx = mesh->mw + 1;
+
+    for (y = 0; y < mesh->mh; y++)
+    {
+        pt = &mesh->pmspt[cidx];
+
+        for (x = 0; x < mesh->mw; x++, pt++)
+        {
+            spr->px0 = pt[0].x + pt[0].ofsx;
+            spr->py0 = pt[0].y + pt[0].ofsy;
+
+            spr->px1 = pt[1].x + pt[1].ofsx;
+            spr->py1 = pt[1].y + pt[1].ofsy;
+
+            spr->px2 = pt[lidx].x + pt[lidx].ofsx;
+            spr->py2 = pt[lidx].y + pt[lidx].ofsy;
+
+            spr->px3 = pt[lidx + 1].x + pt[lidx + 1].ofsx;
+            spr->py3 = pt[lidx + 1].y + pt[lidx + 1].ofsy;
+
+            PkPolyF4_Add(pk, spr, 0);
+        }
+
+        cidx += lidx;
+    }
+}
+
+void PkFTMesh_Add(SPR_PKT pk, SPR_PRM *spr, PKMESH *mesh)
+{
+    int     cidx, lidx;
+    int     x, y;
+    PKMSPT *pt;
+
+    cidx = 0;
+    lidx = mesh->mw + 1;
+
+    for (y = 0; y < mesh->mh; y++)
+    {
+        pt = &mesh->pmspt[cidx];
+
+        for (x = 0; x < mesh->mw; x++, pt++)
+        {
+            spr->ux  = pt[0].u;
+            spr->uy  = pt[0].v;
+
+            spr->uw  = pt[1].u    - pt[0].u;
+            spr->uh  = pt[lidx].v - pt[0].v;
+
+            spr->px0 = pt[0].x + pt[0].ofsx;
+            spr->py0 = pt[0].y + pt[0].ofsy;
+
+            spr->px1 = pt[1].x + pt[1].ofsx;
+            spr->py1 = pt[1].y + pt[1].ofsy;
+
+            spr->px2 = pt[lidx].x + pt[lidx].ofsx;
+            spr->py2 = pt[lidx].y + pt[lidx].ofsy;
+
+            spr->px3 = pt[lidx + 1].x + pt[lidx + 1].ofsx;
+            spr->py3 = pt[lidx + 1].y + pt[lidx + 1].ofsy;
+
+            PkPolyFT4_Add(pk, spr, 2);
+        }
+
+        cidx += lidx;
+    }
+}
+
+#if 1
 INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_SetHLinOfs);
+#else
+void PkMesh_SetHLinOfs(/* v1 3 */ PKMESH *mesh, /* a3 7 */ int no, /* f12 50 */ float x, /* f13 51 */ float y)
+{
+    /* a0 4 */ PKMSPT *pt;
+    /* v1 3 */ int i;
+
+    if (mesh->mh >= no)
+    {
+        i  = mesh->mw + 1;
+        pt = &mesh->pmspt[i * no];
+
+        /* For loop or while loop? */
+        #if 0
+        for (i; i > 0; i--)
+        {
+            pt->ofsx = x;
+            pt->ofsy = y;
+            pt++;
+        }
+        #else
+        if (i > 0)
+        {
+            while (i != 0)
+            {
+                pt->ofsx = x;
+                pt->ofsy = y;
+
+                pt++;
+                i--;
+            }
+        }
+        #endif
+    }
+}
+#endif
 
 INCLUDE_ASM(const s32, "menu/pksprite", PkMesh_SetVLinOfs);
 
