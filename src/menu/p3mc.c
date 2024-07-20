be49ee6987b2854f3144a720f2807bbf88cf86fc
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2024-07-20 21:09:16 +0200

    menu/p3mc: match functions P3MC_CheckChangeClear P3MC_CheckChangeSet P3MC_CheckIsNewSave P3MC_DeleteDataWork P3MC_GetUserStart P3MC_OpeningCheckStart P3MC_OpeningCheckEnd P3MC_SetUserWorkTime

--- src/menu/p3mc.c
+++ src/menu/p3mc.c
@@ -2,7 +2,10 @@
 
 #include "menu/memc.h"
 
+#include <../libcdvd.h>
+
 #include <stdio.h>
+#include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -21,18 +24,21 @@ int NeedSize[2];
 int UChkSize[2];
 
 /* bss - static */
-char filePath[64];
-MEMC_INFO *mcmenu_info;
-sceMcTblGetDir p3mcTblGetDir[8];
+extern char filePath[64];
+extern MEMC_INFO *mcmenu_info;
+extern sceMcTblGetDir p3mcTblGetDir[8];
 
 /* sbss - static */
 int isFileFlgCash;
 
 /* bss - static */
-u_char McLogFileFlg[80];
-u_char McReplayFileFlg[80];
+extern u_char McLogFileFlg[80];
+extern u_char McReplayFileFlg[80];
 
 static char* _P3MC_GetFilePath(int mode, int fileNo);
+static void  _P3MC_dataCheckFunc(P3MC_WORK *pw, P3MCDataCheckFunc funcp);
+static int   _P3MC_CheckUserData(P3MC_WORK *pw);
+static int   _P3MC_CheckUserDataHead(P3MC_WORK *pw);
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_GetIconSize);
 
@@ -40,7 +46,7 @@ INCLUDE_RODATA(const s32, "menu/p3mc", D_00396180);
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_GetIconPtr);
 
-static void _P3MC_SetUserDirName(/* a0 4 */ int mode, /* a1 5 */ int fileNo)
+static void _P3MC_SetUserDirName(int mode, int fileNo)
 {
     memc_setDirName(_P3MC_GetFilePath(mode, fileNo));
 }
@@ -126,17 +132,78 @@ INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_freesize_chk);
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_CheckChange);
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_CheckChangeClear);
+void P3MC_CheckChangeClear(void)
+{
+    memc_setChangeState(0);
+}
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_CheckChangeSet);
+void P3MC_CheckChangeSet(void)
+{
+    memc_setChangeState(1);
+}
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_CheckIsNewSave);
+int P3MC_CheckIsNewSave(int mode)
+{
+    return FreeSizeFlg & mode;
+}
 
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_GetSaveDataSize);
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_DeleteDataWork);
+void P3MC_DeleteDataWork(MCRWDATA_HDL *phdl)
+{
+    if (phdl == NULL)
+        return;
+    
+    if (phdl->pMemTop != NULL)
+        free(phdl->pMemTop);
+    
+    free(phdl);
+}
 
+#define USER_HDR(x) ((USER_HEADER*)x)
+
+#if 1
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_MakeDataWork);
+#else
+MCRWDATA_HDL* P3MC_MakeDataWork(/* s4 20 */ int dsize, /* s5 21 */ USER_DATA *puser)
+{
+    /* s3 19 */ MCRWDATA_HDL *phdl;
+    /* s2 18 */ u_char *pdata;
+    /* s1 17 */ int asize;
+    /* s0 16 */ int dsize0;
+
+    phdl = malloc(sizeof(MCRWDATA_HDL));
+
+    if (phdl != NULL)
+    {
+        dsize0 = ((dsize + 15) >> 4) * 16;
+
+        pdata = memalign(16, dsize0 + 0x2694);
+        memset(pdata, 0, dsize0 + 0x2694);
+
+        phdl->pMemTop  = pdata; // 1045
+        phdl->rwsize   = dsize0 + sizeof(USER_HEADER); // 1046
+        phdl->datasize = dsize0 + sizeof(USER_HEADER); // 1047
+        phdl->srcsize  = dsize; // 1048
+
+        phdl->pHead = (USER_HEADER*)pdata; // 1050
+        phdl->pData = pdata + sizeof(USER_HEADER); // 1051
+        phdl->pFoot = USER_HDR(pdata + sizeof(USER_HEADER))->footer[dsize0]; // 1052
+
+        if (puser != NULL)
+        {
+            USER_HDR(pdata)->user = *puser;
+        }
+
+        return phdl;
+    }
+    else
+    {
+        P3MC_DeleteDataWork(NULL);
+        return NULL;
+    }
+}
+#endif
 
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MCStrCmpLen);
 
@@ -144,7 +211,45 @@ INCLUDE_ASM(const s32, "menu/p3mc", _P3MCStrNum);
 
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_MemcCheck);
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_GetUserStart);
+int P3MC_GetUserStart(int mode, P3MC_USRLST *pUsrLst, int bFirst)
+{
+    GETUSER_WORK *pWork;
+
+    if (pUChkWork != NULL)
+        free(pUChkWork);
+    
+    pWork = memalign(16, sizeof(GETUSER_WORK));
+    pUChkWork = pWork;
+
+    portCheckFlg = 0;
+
+    if (pWork != NULL)
+    {
+        pWork->bFirst = bFirst;
+        pWork->curState = 0;
+
+        if (mode & 1)
+        {
+            pWork->curMode = 1;
+        }
+        else if (mode & 2)
+        {
+            pWork->curMode = 2;
+        }  
+        else
+        {
+            printf("P3MC_GetUser Error Mode is unknown!\n");
+            pWork->curMode = 1;
+        }
+            
+
+        pWork->curFno = 0;
+        pWork->curUserMode = mode;
+        pWork->pUserLst = pUsrLst;
+    }
+
+    return -1;
+}
 
 void P3MC_GetUserEnd(void)
 {
@@ -164,9 +269,30 @@ INCLUDE_ASM(const s32, "menu/p3mc", P3MC_SortUser);
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_CheckBrokenUser);
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_OpeningCheckStart);
+void P3MC_OpeningCheckStart(void)
+{
+    GETUSER_WORK *pWork;
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_OpeningCheckEnd);
+    if (pUChkWork != NULL)
+        free(pUChkWork);
+
+    pWork = memalign(16, sizeof(GETUSER_WORK));
+    pUChkWork = pWork;
+    
+    P3MC_CheckChangeSet();
+    portCheckFlg = 0;
+
+    if (pWork != NULL)
+        pWork->curState = 0;    
+}
+
+void P3MC_OpeningCheckEnd(void)
+{
+    if (pUChkWork != NULL)
+        free(pUChkWork);
+
+    pUChkWork = NULL;
+}
 
 INCLUDE_RODATA(const s32, "menu/p3mc", D_00396420);
 
@@ -178,13 +304,66 @@ INCLUDE_RODATA(const s32, "menu/p3mc", D_00396498);
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_OpeningCheck);
 
+#if 1
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_LoadUser);
+#else
+int P3MC_LoadUser(/* s5 21 */ int mode, /* s3 19 */ int fileNo, /* s1 17 */ MCRWDATA_HDL *pdhdl, /* s4 20 */ int flg)
+{
+    P3MC_WORK *pw = &P3MC_Work;
+
+    memset(pdhdl->pMemTop, 0, pdhdl->rwsize);
+
+    _P3MC_SetUserDirName(mode, fileNo);
+    _P3MC_dataCheckFunc(pw, _P3MC_CheckUserData);
+
+    pw->prgflag    = flg; // 2114
+
+    pw->dstat      = 0; // 2115
+    pw->dhdl       = pdhdl; // 2121
+    
+    pw->data_mode  = mode; // 2117
+    pw->data_no    = fileNo; // 2118
+    pw->data_stage = 0; // 2119
+
+    pw->prg = 0x1000; // 2122
+    return 0;
+}
+#endif
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_LoadCheck);
 
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_loadCheck);
 
-INCLUDE_ASM(const s32, "menu/p3mc", P3MC_SetUserWorkTime);
+void P3MC_SetUserWorkTime(USER_DATA *puser)
+{
+    int        err;
+    sceCdCLOCK clock;
+
+    err = sceCdReadClock(&clock);
+
+    puser->date_pad = rand() % 200;
+
+    if (err != 0 && clock.stat == 0)
+    {
+        puser->date_second = clock.second;
+        puser->date_minute = clock.minute;
+        puser->date_hour   = clock.hour;
+
+        puser->date_day    = clock.day;
+        puser->date_month  = clock.month;
+        puser->date_year   = clock.year + 0x2000;
+    }
+    else
+    {
+        puser->date_second = 0;
+        puser->date_minute = 0;
+        puser->date_hour   = 12;
+
+        puser->date_day    = 1;
+        puser->date_month  = 1;
+        puser->date_year   = 0x2000;
+    }
+}
 
 INCLUDE_ASM(const s32, "menu/p3mc", P3MC_SaveUser);
 
@@ -194,21 +373,54 @@ INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_SaveCheck);
 
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_proc);
 
-void _P3MC_dataCheckFunc(P3MC_WORK *pw,P3MCDataCheckFunc funcp)
+void _P3MC_dataCheckFunc(P3MC_WORK *pw, P3MCDataCheckFunc funcp)
 {
     pw->data_cfunc = funcp;
 }
 
-// INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_CheckUserData);
-static int _P3MC_CheckUserData(/* a0 4 */ P3MC_WORK *pw)
+static int _P3MC_CheckUserData(P3MC_WORK *pw)
 {
-    /* s0 16 */ USER_FOOTER *pfoot = pw->dhdl->pFoot;
+    USER_FOOTER *pfoot = pw->dhdl->pFoot;
 
     if (_P3MC_CheckUserDataHead(pw))
         return 1;
     else 
         return (strcmp(FooterID, pfoot->footer) != 0);
-    
 }
 
+#if 1
 INCLUDE_ASM(const s32, "menu/p3mc", _P3MC_CheckUserDataHead);
+#else
+static int _P3MC_CheckUserDataHead(/* s1 17 */ P3MC_WORK *pw)
+{
+	/* s0 16 */ USER_HEADER *hed = (USER_HEADER*)pw->dhdl->pMemTop;
+
+    if (strcmp(hed->header, HedderID) == 0)
+    {
+        if (strcmp(hed->footer, FooterID) == 0)
+        {
+            if (!hed->user.flg)
+                return 0;
+
+            if (hed->user.fileNo == pw->data_no)
+            {
+                if (hed->user.mode == pw->data_mode)
+                {
+                    if (pw->data_stage > 0)
+                    {
+                        if (hed->user.stageNo != pw->data_stage)
+                            return 1;
+                    }
+
+                    if (hed->user.name[0] != 0)
+                        return (hed->user.name1[0] == 0);
+                    else
+                        return 0;
+                }
+            }
+        }
+    }
+    
+    return 1;
+}
+#endif
