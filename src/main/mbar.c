9fba199cf27b0fa2f5430ce6ab1899e52d9feaac
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2024-09-22 13:26:43 +0200

    main: split `mbar.c`

---
+++ src/main/mbar.c
@@ -0,0 +1,184 @@
+#include "common.h"
+
+INCLUDE_ASM("main/mbar", examCharSet);
+
+INCLUDE_ASM("main/mbar", clrColorBuffer);
+
+INCLUDE_ASM("main/mbar", MbarMemberClear);
+
+INCLUDE_ASM("main/mbar", examCharBasic);
+
+INCLUDE_ASM("main/mbar", examCharScaleSet);
+
+INCLUDE_ASM("main/mbar", examCharCltSet);
+
+INCLUDE_ASM("main/mbar", examCharPosSet);
+
+INCLUDE_ASM("main/mbar", examCharUVWHSet);
+
+INCLUDE_ASM("main/mbar", examCharAlphaSet);
+
+INCLUDE_ASM("main/mbar", examCharKidoSet);
+
+INCLUDE_ASM("main/mbar", NikoReset);
+
+INCLUDE_ASM("main/mbar", MbarNikoHookUse);
+
+INCLUDE_ASM("main/mbar", MbarNikoVsUse);
+
+INCLUDE_ASM("main/mbar", MbarNikoUnUse);
+
+INCLUDE_ASM("main/mbar", MbarNikoSet);
+
+INCLUDE_ASM("main/mbar", MbarNikoDisp);
+
+INCLUDE_ASM("main/mbar", MbarHookUseInit);
+
+INCLUDE_ASM("main/mbar", MbarHookUnUse);
+
+INCLUDE_ASM("main/mbar", MbarHookUseOK);
+
+INCLUDE_ASM("main/mbar", MbarHookUseNG);
+
+INCLUDE_ASM("main/mbar", MbarHookPoll);
+
+INCLUDE_ASM("main/mbar", vsAnimationInit);
+
+INCLUDE_ASM("main/mbar", vsAnimationReq);
+
+INCLUDE_ASM("main/mbar", vsAnimationReset);
+
+INCLUDE_ASM("main/mbar", vsScr2Move);
+
+INCLUDE_ASM("main/mbar", vsAnimationPoll);
+
+INCLUDE_ASM("main/mbar", metColorInit);
+
+INCLUDE_ASM("main/mbar", metColorSet);
+
+INCLUDE_ASM("main/mbar", metFrameInit);
+
+INCLUDE_ASM("main/mbar", conditionFrameInit);
+
+INCLUDE_ASM("main/mbar", ExamDispInit);
+
+INCLUDE_ASM("main/mbar", ExamDispPlySet);
+
+INCLUDE_ASM("main/mbar", ExamDispReq);
+
+INCLUDE_ASM("main/mbar", ExamDispReset);
+
+INCLUDE_ASM("main/mbar", examScore2Level);
+
+INCLUDE_ASM("main/mbar", ExamDispOn);
+
+INCLUDE_ASM("main/mbar", hex2dec_mbar_tmp);
+
+INCLUDE_ASM("main/mbar", hex2decPlMi);
+
+INCLUDE_ASM("main/mbar", examNumDisp);
+
+INCLUDE_ASM("main/mbar", examScoreSet);
+
+INCLUDE_ASM("main/mbar", examLevelDisp);
+
+INCLUDE_ASM("main/mbar", ExamDispSet);
+
+void ExamDispSubt(void)
+{
+    // Empty
+}
+
+INCLUDE_ASM("main/mbar", MbarInit);
+
+INCLUDE_ASM("main/mbar", MbarReset);
+
+INCLUDE_RODATA("main/mbar", D_00393440);
+
+INCLUDE_RODATA("main/mbar", D_00393448);
+
+INCLUDE_RODATA("main/mbar", D_00393450);
+
+INCLUDE_RODATA("main/mbar", D_00393458);
+
+INCLUDE_ASM("main/mbar", MbarReq);
+
+INCLUDE_ASM("main/mbar", MbarSetCtrlTime);
+
+INCLUDE_ASM("main/mbar", MbarCl1CharSet);
+
+INCLUDE_ASM("main/mbar", MbarCharSetSub);
+
+INCLUDE_ASM("main/mbar", MbarGifInit);
+
+INCLUDE_ASM("main/mbar", MbarGifTrans);
+
+INCLUDE_ASM("main/mbar", MbarCharSet);
+
+INCLUDE_ASM("main/mbar", MbarCharSet2);
+
+INCLUDE_ASM("main/mbar", MbarWindowSet);
+
+INCLUDE_ASM("main/mbar", MbarGetDispPosX);
+
+INCLUDE_ASM("main/mbar", MbarGetDispPosY);
+
+INCLUDE_ASM("main/mbar", MbarGetTimeArea);
+
+INCLUDE_ASM("main/mbar", MbarGetTimeArea2);
+
+INCLUDE_ASM("main/mbar", MbarGetStartTime);
+
+INCLUDE_ASM("main/mbar", MbarGetEndTime);
+
+INCLUDE_ASM("main/mbar", MbarGetStartTap);
+
+INCLUDE_ASM("main/mbar", MbarSclRotMake);
+
+INCLUDE_ASM("main/mbar", MbarGuideLightMake);
+
+INCLUDE_ASM("main/mbar", MbarFlashMake);
+
+INCLUDE_ASM("main/mbar", MbarBackSet);
+
+INCLUDE_RODATA("main/mbar", D_00393488);
+
+INCLUDE_RODATA("main/mbar", D_003934A0);
+
+INCLUDE_ASM("main/mbar", MbarOthSet);
+
+INCLUDE_ASM("main/mbar", MbarCurSet);
+
+INCLUDE_ASM("main/mbar", MbarTapSubt);
+
+INCLUDE_ASM("main/mbar", MbarPosOffsetSet);
+
+INCLUDE_ASM("main/mbar", mbar_othon_frame_set);
+
+INCLUDE_ASM("main/mbar", MbarDisp);
+
+INCLUDE_ASM("main/mbar", MbarDispScene);
+
+INCLUDE_ASM("main/mbar", MbarDispSceneDraw);
+
+INCLUDE_ASM("main/mbar", MbarDispSceneVsDraw);
+
+INCLUDE_ASM("main/mbar", MbarDispSceneVsDrawInit);
+
+INCLUDE_ASM("main/mbar", guidisp_init_pr);
+
+INCLUDE_ASM("main/mbar", guidisp_draw_quit);
+
+INCLUDE_ASM("main/mbar", MbarDispGuiScene);
+
+INCLUDE_ASM("main/mbar", MbarDispGuiSceneMbarArea);
+
+INCLUDE_ASM("main/mbar", lessonTim2InfoGet);
+
+INCLUDE_ASM("main/mbar", lessonCl2InfoGet);
+
+INCLUDE_ASM("main/mbar", MbarDemoCharDisp);
+
+INCLUDE_RODATA("main/mbar", D_003934E8);
+
+INCLUDE_RODATA("main/mbar", D_00393500);
