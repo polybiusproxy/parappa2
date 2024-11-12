f22b6e292120f836432599f3d78dc9eadffa6bac
polybiusproxy <47796739+polybiusproxy@users.noreply.github.com>
2024-11-12 18:44:56 +0100

    prlib/setpointer: split

---
+++ src/prlib/setpointer.cpp
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("prlib/setpointer", ChangePointer__13SpmFileHeader);
+
+INCLUDE_ASM("prlib/setpointer", ChangePointer__7SpmNodeP13SpmFileHeaderP7SpmNode);
+
+INCLUDE_ASM("prlib/setpointer", ChangePointer__13SpaFileHeader);
+
+INCLUDE_ASM("prlib/setpointer", ChangePointer__16SpaNodeAnimationP13SpaFileHeader);
+
+INCLUDE_ASM("prlib/setpointer", ChangePointer__13SpcFileHeader);
